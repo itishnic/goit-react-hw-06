137b510fd67a0f4e48ebc29731ac873ab35809a7
itishnic <122455719+itishnic@users.noreply.github.com>
2024-07-12 17:51:36 +0300

    commit

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,11 +1,8 @@
 import { createSlice,  } from "@reduxjs/toolkit";
 
-const contactsInitialState = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+const contactsInitialState = {
+  items: [],
+};
 
 
 
@@ -14,11 +11,11 @@ const contactsSlice = createSlice({
   initialState: contactsInitialState,
   reducers: {
     deleteContact: (state, action) => {
-      const index = state.findIndex((task) => task.id === action.payload);
-      state.splice(index, 1);
+      const index = state.items.findIndex((item) => item.id === action.payload);
+      state.items.splice(index, 1);
     },
     addContact: (state, action) => {
-      state.push(action.payload);
+      state.items.push(action.payload);
     },
   },
 })
